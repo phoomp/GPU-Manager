c5e838f665c40a5f1eaf7ff9b716bd7840b28f0a
Phoom Punpeng <phoomp14@gmail.com>
2022-07-08 14:03:01 +0700

    Initial Commit

---
+++ Vision GPU Manager/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Vision GPU Manager
+//
+//  Created by Phoom Punpeng on 8/7/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vision GPU Manager/Vision_GPU_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_GPU_ManagerApp.swift
+//  Vision GPU Manager
+//
+//  Created by Phoom Punpeng on 8/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Vision_GPU_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
